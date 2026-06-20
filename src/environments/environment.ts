461c563c0203ae104e8fe695c6cf8d760e53b3e9
Cristian Alexander <caguilarzuniga7@gmail.com>
2026-06-20 03:04:07 -0600

    cambio de url de la api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,7 @@ const appPrefix: string = "donate";
 
 export const environment = {
     pathLogin: '/auth/login',
-    api: 'http://localhost:5017',
-    //api: 'http://192.168.1.3:45455',
+    api: 'https://proyectodonacionapi-production.up.railway.app',
     tokenName: `${appPrefix}-token`,
     applicationName: 'Sistema de Donaciones',
 };
